dde75a0b5a6c6a20fe4f42259389fbfc1d5c441a
Gaurav Tanwar <gaurav.tanwar19@bvicam.in>
2021-05-03 20:02:32 +0530

    Add New Emoji

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -19,6 +19,48 @@ const emojipedia = [
     name: "Rolling On The Floor, Laughing",
     meaning:
       "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😅",
+    name: "Grinning Face with Sweat",
+    meaning:
+      " Intended to depict nerves or discomfort but commonly used to express a close call, as if saying Whew! and wiping sweat from the forehead. "
+  },
+  {
+    id: 5,
+    emoji: "😍",
+    name: "Smiling Face with Heart-Eyes",
+    meaning:
+      "Often conveys enthusiastic feelings of love, infatuation, and adoration, e.g., I love/am in love with this person or thing."
+  },
+  {
+    id: 6,
+    emoji: "🤑",
+    name: "Money-Mouth Face",
+    meaning:
+      "Used for a wide range of content dealing with money, including: making money, loving wealth, being or feeling rich"
+  },
+  {
+    id: 7,
+    emoji: "🤑",
+    name: "Money-Mouth Face",
+    meaning:
+      "Used for a wide range of content dealing with money, including: making money, loving wealth, being or feeling rich"
+  },
+  {
+    id: 8,
+    emoji: "😐",
+    name: "Neutral Face",
+    meaning:
+      "often used to convey mild irritation and concern or a deadpan sense of humor."
+  },
+  {
+    id: 8,
+    emoji: "😶",
+    name: "Face Without Mouth",
+    meaning:
+      "Meaning widely varies, but commonly conveys speechlessness. May also convey moderately negative emotions, such as disappointment. "
   }
 ];
 
