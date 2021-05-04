9e4ad94533c7b80c89f02eca9934cc1ea552cf5d
Gaurav Tanwar <gaurav.tanwar19@bvicam.in>
2021-05-04 13:08:58 +0530

    Updates

---
+++ precache-manifest.0b98f90767a8a8d819328391f58ef1aa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "effd6fd8076eed0fac3066f055cc4c9b",
+    "url": "/Emoji-pedia/index.html"
+  },
+  {
+    "revision": "d478e7f699f8a4fe0e4f",
+    "url": "/Emoji-pedia/static/js/2.fc726699.chunk.js"
+  },
+  {
+    "revision": "48937a40deb1e97fb947",
+    "url": "/Emoji-pedia/static/js/main.df4a65b4.chunk.js"
+  },
+  {
+    "revision": "37980c36c0ce6a9d5dfd",
+    "url": "/Emoji-pedia/static/js/runtime-main.a619f6af.js"
+  }
+]);
